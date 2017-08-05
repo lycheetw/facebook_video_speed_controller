c4fbb2ff877b8ee1e15d42302a92be0a464973b7
lycheetw <lychee.tw+github@gmail.com>
2017-08-05 16:03:28 +0800

    Fix bug and refactor

--- main.js
+++ main.js
@@ -1,15 +1,17 @@
-const selectedClassName = "_2xyd"
+const selectedClassName = "_2xyd";
 
 let MutationObserver = window.MutationObserver || window.WebKitMutationObserver;
 
 let observer = new MutationObserver(mutations => {
   mutations.forEach(async function(mutation) {
-      if(mutation.target.id.length === 0) return;
-      let block = await findBlock(mutation.target.id);
-      if(block === null) return;
+      if(mutation.target.nodeName !== "VIDEO") return;
+      if(mutation.target.hasAttribute("_mark")) return;
 
-      let settingBlock = getSettingBlock(block);
-      
+      let video = mutation.target;
+      let container = video.parentNode;
+      let settingBlock = getSettingBlock(container);
+      if(settingBlock === null) return;
+
       let rateList = [0.5, 0.75, 1, 1.25, 1.5, 2];
 
       //Add rate button into setting block
@@ -18,47 +20,32 @@ let observer = new MutationObserver(mutations => {
         a.setAttribute("rate", rate);
         a.className = "_4v8v";
         a.innerHTML = `<div class="_2xyg"><div class="${rate === 1 ? selectedClassName: ""}"></div></div><div class="_4v8x">${rate}x</div>`;
-        a.addEventListener("click", () => setRate(block, rate));
+        a.addEventListener("click", () => setRate(container, rate));
         
         let len = settingBlock.childNodes.length;
         settingBlock.insertBefore(a, settingBlock.childNodes[len - 1]);
       }
       //Mark this video has been processed
-      getVideoBlock(block).setAttribute("_mark", 1);
+      video.setAttribute("_mark", 1);
   });    
-})
-
-async function findBlock(blockId) {
-  return new Promise(resolve => {
-    setTimeout(() => {
-      let block = document.querySelector(`#${blockId}`);
-      if( block === null
-        || getVideoBlock(block) === null 
-        || getVideoBlock(block).hasAttribute("_mark")) {
-        resolve(null);
-      } else {
-        resolve(block);
-      }
-    }, 500)
-  });
-}
+});
 
 function setRate(block, rate){
   getVideoBlock(block).playbackRate = rate;
   let nodes = getSettingBlock(block).querySelectorAll("a[rate]");
   for(let n of nodes){
-    if(n.getAttribute("rate") == rate) n.childNodes[0].childNodes[0].className = selectedClassName
-    else n.childNodes[0].childNodes[0].className = ""
+    if(n.getAttribute("rate") == rate) n.childNodes[0].childNodes[0].className = selectedClassName;
+    else n.childNodes[0].childNodes[0].className = "";
   }
 }
 
 function getVideoBlock(block){
-  return block.querySelector("video")
+  return block.querySelector("video");
 }
 
 function getSettingBlock(block){
-  return block.querySelector("._28h1")
+  return block.querySelector("._28h1");
 }
 
-const config = {  childList: true, subtree: true };
+const config = {  childList: true, subtree: true, characterData: true, attributes: true };
 observer.observe(document, config);
